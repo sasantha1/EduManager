da97f03d9ded7d9824230b2ce4d7acc7fcbfa32e
Susitharathnayaka <srilankanrawds@gmail.com>
2025-05-18 22:45:55 +0530

    Add files via upload

---
+++ src/main/java/com/sms/dto/request/StudentUpdateRequest.java
@@ -0,0 +1,35 @@
+package com.sms.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentUpdateRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Student ID is required")
+    @Pattern(regexp = "^S\\d{4,}$", message = "Student ID must start with 'S' followed by at least 4 digits")
+    private String studentId;
+
+    @NotBlank(message = "Program is required")
+    private String program;
+
+    @NotBlank(message = "Year is required")
+    private String year;
+}
